c28bef6e2b35dbd58dddd2b6f33f1552df43b3c0
Julian Maurice <julian.maurice@biblibre.com>
2021-02-11 11:39:01 +0100

    Allow everyone to change locale

--- Module.php
+++ Module.php
@@ -3,11 +3,20 @@
 namespace LocaleSwitch;
 
 use Omeka\Module\AbstractModule;
+use Zend\Mvc\MvcEvent;
 
 class Module extends AbstractModule
 {
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function onBootstrap(MvcEvent $event)
+    {
+        parent::onBootstrap($event);
+
+        $acl = $this->getServiceLocator()->get('Omeka\Acl');
+        $acl->allow(null, 'LocaleSwitch\Controller\Site\Index');
     }
 }
